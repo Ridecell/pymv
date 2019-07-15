cde0705b910f895043e608a7bee3a4c78ce46a0d
John Miller <personalcomputer@users.noreply.github.com>
2019-07-15 00:32:02 -0700

    fix sys.path insert, add --dry-run arg, minor refactoring

--- pymove.py
+++ pymove.py
@@ -1,29 +1,46 @@
+#!/usr/bin/env python2
 import os
 import sys
+import argparse
 
-sys.path.insert(0, os.path.join(os.path.dirname(__file__), 'rope_vendored'))
+sys.path.insert(0, os.path.join(os.path.dirname(os.path.realpath(__file__)), 'rope_vendored'))
 
 import rope.base.project
 from rope.base import libutils
 from rope.refactor.move import MoveModule
 
 
-cwd = '.'
-src = sys.argv[1]
-dest = sys.argv[2]
+PROJECT_DIR = '.'
 
-# cwd = os.path.abspath(cwd)
-# src = os.path.join(cwd, src)
-# dest = os.path.join(cwd, dest)
 
-print(repr((cwd, src, dest)))
+def move(project, src, dest, dry_run=False):
+    project = rope.base.project.Project(PROJECT_DIR)
+    resource = libutils.path_to_resource(project, src)
+    resource2 = libutils.path_to_resource(project, dest, type='folder' if resource.is_folder() else 'file')
 
-project = rope.base.project.Project(cwd)
-resource = libutils.path_to_resource(project, src)
-resource2 = libutils.path_to_resource(project, dest, type='folder' if resource.is_folder() else 'file')
+    mover = MoveModule(project, resource)
 
-mover = MoveModule(project, resource)
+    changeset = mover.get_changes(resource2)
+    if dry_run:
+        print(str(changeset.get_description()))
+    else:
+        project.do(changeset)
 
-print(str(mover.get_changes(resource2).get_description()))
 
-# project.do(mover.get_changes(destination))
+def main():
+    arg_parser = argparse.ArgumentParser(description='Move python files and folders, automatically updating import '
+                                                     'statements in dependent files.')
+    arg_parser.add_argument('source_path', type=str)
+    arg_parser.add_argument('destination_path', type=str)
+    arg_parser.add_argument('--dry-run', action='store_true', dest='dry_run')
+    args = arg_parser.parse_args(sys.argv[1:])
+
+    # project_dir = os.path.abspath(PROJECT_DIR)
+    # source_path = os.path.join(PROJECT_DIR, args.source_path)
+    # destination_path = os.path.join(PROJECT_DIR, args.destination_path)
+
+    move(PROJECT_DIR, args.source_path, args.destination_path, dry_run=args.dry_run)
+
+
+if __name__ == '__main__':
+    main()
